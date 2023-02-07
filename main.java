fcdbb5e3211c034e31069d909ec226e7831e9e56
Silv <91438536+Polsiv@users.noreply.github.com>
2023-02-07 14:04:49 -0500

    update

--- main.java
+++ main.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+ //works for inputs
+
+//java = package; util = sub-package
+
+
 class Main{
 
-public static void main (String[] args){
+    public static void main (String[] args){
+    System.out.println("HELLO WORLD");
 
-        System.out.println("HELLO WORLD");
+//system is a class where it has all the access to the hardware
 
-    }
+    Scanner keyboard = new Scanner(System.in);
 
+    
+    
+   }    
 }
