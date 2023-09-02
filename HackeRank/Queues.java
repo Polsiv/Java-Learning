69c4cae6b053d35bd07881ebbbaeeeaae7db250d
Silv <forenks1005@gmail.com>
2023-09-02 17:16:19 -0500

    Appending new java files

---
+++ HackeRank/Queues.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Queues{
+    public static void main(String[] args) {
+        
+        Queue line = new LinkedList();
+
+        //Adding elements to the queue
+        line.add("Jean");
+        line.add("Jocksan");
+        line.add("Susan");
+
+        System.out.println(line);
+
+
+    }
+}
