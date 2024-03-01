91331c7d5e34201d341a3cc8104984b08173c735
Silv <forenks1005@gmail.com>
2024-03-01 05:37:55 -0500

    .

--- HackeRank/test.java
+++ HackeRank/test.java
@@ -5,22 +5,19 @@ import java.util.*;
         Scanner in = new Scanner(System.in);
         Deque<Integer> deque = new ArrayDeque<Integer>();
         HashMap<Integer, Integer> counts = new HashMap<>();
-
-        int[] numbers = {5, 3, 5, 2, 3, 2};
             
-        int n = 6;
-        int m = 3;
+        int n = in.nextInt();
+        int m = in.nextInt();
         int max = 0;
             
         for (int i = 0; i < n; i++) {
-
                 
-            int num = numbers[i];
+            int num = in.nextInt();
             deque.add(num);   
             int count = counts.getOrDefault(num, 0);
             counts.put(num, ++count);
-            //System.out.println(counts);
-                
+            System.out.println(counts);
+            
             if(i >= m-1){
                 if(counts.size() > max){
                     //System.out.println("counts " + counts.size());
@@ -31,7 +28,7 @@ import java.util.*;
                 int removed = deque.remove();
                 int removingnumber = counts.get(removed);
                 removingnumber--;
-                
+             
                 if(removingnumber == 0){
                     counts.remove(removed);
                 } else {
